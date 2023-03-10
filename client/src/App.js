07979bd1fa2dd0a2455bb2e40c65a02a65da37ce
Ricardo Estrada Villavicencio <rickyv815@gmail.com>
2023-03-10 10:57:18 -0700

    updated App.js

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Ricardo Estrada Villavicencio
         </a>
       </header>
     </div>
